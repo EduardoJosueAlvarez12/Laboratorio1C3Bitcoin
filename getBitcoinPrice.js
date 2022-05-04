f12bf8bd99b8f0e2a8fbda37cc599c95631e3fc7
EduardoJosueAlvarez12 <SMIS010720@ugb.edu.sv>
2022-05-04 16:54:05 -0600

    Lab 1

--- getBitcoinPrice.js
+++ getBitcoinPrice.js
@@ -5,14 +5,19 @@ axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
 .then((response) => {
     console.log('Se extrajo correctamente el precio de bitcoin');
     let bitcoinData = '';
-    let data = `[${[response.data.bpi]}]`
-    console.log(data);
-    console.log(response.data.bpi)
-    response.data.bpi.forEach(row => {
-        bitcoinData += `${row.USD['code']}, ${row['symbol']}, ${row['rate']}, ${row['description']}, ${row['rate_float']}\n`;
+    
+    var items = [];
+    for (key in response.data.bpi) {
+        items.push(response.data.bpi[key])
+    }
+
+    console.log(items)
+
+    items.forEach(row => {
+        bitcoinData += `${row['code']}, ${row['symbol']}, ${row['rate']}, ${row['description']}, ${row['rate_float']}\n`;
     });
 
-    return fs.writeFile('bitcoinPrice.csv');
+    return fs.writeFile('bitcoinPrice.txt', bitcoinData);
 })
 .then(() => {
     console.log('Se guardo el precio del bitcoin en bitcoinPrice.csv');
